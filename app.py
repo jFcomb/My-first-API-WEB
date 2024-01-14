afb7f1905fcd8ec3c6f3dde5c4345f2153da8300
jfcomb <mbjfco@gmail.com>
2024-01-14 12:24:32 -0600

    version 1.1

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 import streamlit as st
+import scipy.stats
+import time
 
 st.header('Lanzamiento de monedas')
 
+# Agregamos un chart
+chart = st.line_chart([0.5])
+
+#Función de emulación de lanzamiento de moneda
+def toss_coin(n):
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcoe_1_count = 0
+    
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_nochart.add_rows([mean])
+        chart.add_rows([mean])
+        time_sleep(0.05)
+        
+    return mean
+
+        
 # Dibujamos el Slider
 number_of_trials = st.slider("¿Cuantos intentos quieres realizar?", 1,1000,10)
 
@@ -10,5 +34,4 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso')
-    
-st.write('Esta aplicación aún no es funcional. En construcción.')
+    mean = toss_coin(number_of_trials)

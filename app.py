deb6fcf67066c7ed5d3dd43d55c71c1c444fb7d8
jfcomb <mbjfco@gmail.com>
2024-01-14 17:56:42 -0600

    version 1

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import pandas as pd
 import streamlit as st
 import scipy.stats
 import time
+
+# Variables de estado al ejecutar streamlit
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+    
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiments_results'] = pd.DataFrame(columns = ['no', 'iteraciones', 'media'])
 
 st.header('Lanzamiento de monedas')
 
@@ -34,4 +42,16 @@ start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([st.session_state['df_experiment_results'], 
+                                                           pd.DataFrame(data=[[st.session_state['experiment_no'],
+                                                                               number_of_trials,
+                                                                               mean]],
+                                                                        columns=['no', 'iterations', 'mean'])
+                                                           ],
+                                                          axis=0)
+    st.session_state['df_experiment_results'] = \\
+        st.session_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.session_state['df_experiment_results'])

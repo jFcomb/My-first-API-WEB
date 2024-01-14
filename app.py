3d8333a2b8d733d6f24c76d2088658347f5afdbd
jfcomb <mbjfco@gmail.com>
2024-01-13 22:41:56 -0600

    app code addition

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
-st.header('Lanzar una moneda')
+st.header('Lanzamiento de monedas')
 
+# Dibujamos el Slider
+number_of_trials = st.slider("¿Cuantos intentos quieres realizar?", 1,1000,10)
+
+# Dibujamos el botón
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso')
+    
 st.write('Esta aplicación aún no es funcional. En construcción.')
